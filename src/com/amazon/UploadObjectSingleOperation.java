e94d21791b84ede4b1be3192f47b3bf3a61adcd0
samuel.silva <samsfire7@hotmail.com>
2018-02-02 00:12:26 -0200

    Finally i did it!! The Amazon conection it's working.

--- src/com/amazon/UploadObjectSingleOperation.java
+++ src/com/amazon/UploadObjectSingleOperation.java
@@ -9,21 +9,27 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class UploadObjectSingleOperation {
-	private static String bucketName = "videosamba";
-	private static String keyName = "AKIAIZVFBD7E63TFX2EA";
-	private static String uploadFileName = "";
+	private static String bucketInputName = "videosamba/input";
+	//private static String bucketOutputName = "videosamba/output";
+	//private static String keyName = "video";
 	
-	public void upload() throws IOException {
-		AWSCredentials credentials = new BasicAWSCredentials("AKIAIYWBURTWL2WSLNAQ", "/fiCK7x8j0aFGdx0Q0QlOEWPb3eJQvmxN+VNFXIw");
-		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+	public void upload(File file) throws IOException {
+		AWSCredentials credentials = new BasicAWSCredentials("AKIAI6P2QTS6BRVAUZPQ", "x5fWrkFrOd5zIMhwuWMkGwI5ZcSed7ok9WkAHxyW");
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion("us-east-2").withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
         
         try {
-            File file = new File(uploadFileName);
-            s3Client.putObject(new PutObjectRequest(bucketName, keyName, file));
+			s3Client.putObject(new PutObjectRequest(bucketInputName, file.getName(), file));
+        	
+        	//S3Object s3object = s3Client.getObject(new GetObjectRequest(bucketOutputName, keyName));
+        	System.out.println("teste");
 
          } catch (AmazonServiceException ase) {
             System.out.println("Error Message:    " + ase.getMessage());
--- src/com/bean/UploadBean.java
+++ src/com/bean/UploadBean.java
@@ -1,17 +1,38 @@
 package com.bean;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.UploadedFile;
+
+import com.amazon.UploadObjectSingleOperation;
 
 @ManagedBean(name = "upload")
 public class UploadBean {
 
-	private String value = "Teste";
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
+	private UploadedFile file;
+	
+	public void uploadVideo() throws IOException {
+        if(file != null) {
+        	UploadObjectSingleOperation objectUpload = new UploadObjectSingleOperation();
+        	
+        	InputStream fileInputStream = file.getInputstream();
+        	objectUpload.upload(new File("C:\\Users\\silva\\Downloads\\Video.mp4"));
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+	
+	public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
 }
